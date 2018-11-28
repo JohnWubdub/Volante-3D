2347d0e163f86ddf2c8b09ec5934260e6e2090a5
John <jw4429@nyu.edu>
2018-11-28 21:41:55 +0100

    First release on Itch.io -Fully Functioning Menu -Menu theme of a seagull cawing -Weirdness with the shader in webGL stuff -Posted version to itch.io -all functioning fine
    Problems/Bugs/Goals
    -fix all the bugs and make it actually fun

---
+++ Assets/Scripts/ControlsSceneMan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlsSceneMan : MonoBehaviour {
+
+
+	public void Exit ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+	
+	void Update ()
+    {
+        if(Input.GetKey("escape"))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+	
+	void Update ()
+    {
+		if(Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+	}
+}
